ac5b59cffb0a8c63df4c0d8df15ba7a535925985
Alex Notov <alex@zealoushacker.com>
2021-06-20 18:08:05 -0600

    DRYs up spec

--- library.spec.js
+++ library.spec.js
@@ -1,10 +1,9 @@
+const Book = require('./book.js');
+const Library = require('./library.js');
 describe('Library', () => {
   describe('Given I have a book', () => {
     describe('When I add a book to the library', () => {
       test('Then the book is in the library', () => {
-        const Library = require('./library.js'); 
-        const Book = require('./book.js');
-
         const _library = new Library();
         const _book = new Book();
         
@@ -16,17 +15,17 @@ describe('Library', () => {
   });
 
   describe('Given there are books in the library', () => {
+    let _library, _book1, _book2, _book3;
+    beforeEach(() => {
+      _library = new Library();
+
+      _book1 = new Book("War and Peace");
+      _book2 = new Book("Lolita");
+      _book3 = new Book("Brothers Karamzov");
+
+    });
     describe('When I check for the list of books', () => {
       test('Then I see all books returned and sorted alphabetically by title', () => {
-        const Library = require('./library.js');
-        const Book = require('./book.js');
-
-        const _library = new Library();
-
-        const _book1 = new Book("War and Peace");
-        const _book2 = new Book("Lolita");
-        const _book3 = new Book("Brothers Karamzov");
-
         _library.add(_book1);
         _library.add(_book2);
         _library.add(_book3);
@@ -37,38 +36,19 @@ describe('Library', () => {
 
     describe('When I search for a book by title', () => {
       test('Then I get the book of that title', () => {
-        const Library = require('./library.js');
-        const Book = require('./book.js');
-
-        const _library = new Library();
-
-        const _book1 = new Book("War and Peace");
-        const _book2 = new Book("Lolita");
-        const _book3 = new Book("Brothers Karamzov");
-
         _library.add(_book1);
         _library.add(_book2);
         _library.add(_book3);
 
         expect(_library.search('War and Peace')).toEqual(_book1);
       });
-    });
+      test('Then I get the book of that title no matter the order of books added', () => {
+        _library.add(_book2);
+        _library.add(_book3);
+        _library.add(_book1);
 
-    test('Then I get the book of that title no matter the order of books added', () => {
-      const Library = require('./library.js');
-      const Book = require('./book.js');
-
-      const _library = new Library();
-
-      const _book1 = new Book("War and Peace");
-      const _book2 = new Book("Lolita");
-      const _book3 = new Book("Brothers Karamzov");
-
-      _library.add(_book2);
-      _library.add(_book3);
-      _library.add(_book1);
-
-      expect(_library.search('War and Peace')).toEqual(_book1);
+        expect(_library.search('War and Peace')).toEqual(_book1);
+      });
     });
   });
 });
